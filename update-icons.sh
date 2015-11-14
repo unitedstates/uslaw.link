4203bdcb27554c4c89b8647e5e28a79dfa52d93f
Joshua Tauberer <jt@occams.info>
2015-11-13 20:51:04 -0500

    add icons for the sources

---
+++ update-icons.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd public/source-icons
+
+# fetch
+wget -qO usgpo.png http://www.gpo.gov/images/news-media/logo_5415.jpg
+wget -qO house.png https://pbs.twimg.com/profile_images/86836564/ushousereps_400x400.jpg
+wget -qO libraryofcongress.jpeg https://pbs.twimg.com/profile_images/463771973139447808/Gv1mSKiG.jpeg
+wget -qO govtrack.png https://pbs.twimg.com/profile_images/3113926464/808a08bd97e3b46f66c342095a2847e8.png
+wget -qO cornell_lii.png https://pbs.twimg.com/profile_images/489847044786253824/hfokfHCX_200x200.png
+wget -qO courtlistener.png https://pbs.twimg.com/profile_images/487693186022641664/FWfyCie1.png
+
+# resize
+for img in `ls *.png *.jpeg`; do
+    convert $img -geometry 150 /tmp/icon-$$ \
+        && mv /tmp/icon-$$ $img
+done
+
+# update client-side icon list!
