7b52a797e4f766fb4c474a2f02bc5d70643c9bd6
Joshua Tauberer <jt@occams.info>
2019-08-04 08:05:18 -0400

    some links improvements

--- parallel-citations.js
+++ parallel-citations.js
@@ -45,7 +45,6 @@ var fetchers = {
     else
       callback([])
   },
-
   usc: function(usc, cite, env, callback) {
     // Because of the ambiguity of dashes being within section numbers
     // or delineating ranges, we can test if the citation actually exists
@@ -66,6 +65,18 @@ var fetchers = {
     } else {
       callback([])
     }
+  },
+  cfr: function(cfr, cite, env, callback) {
+    if (cfr.links.usgpo)
+      get_from_usgpo_mods(cite, cfr.links.usgpo.mods, callback);
+    else
+      callback([])
+  },
+  fedreg: function(fedreg, cite, env, callback) {
+    if (fedreg.links.usgpo)
+      get_from_usgpo_mods(cite, fedreg.links.usgpo.mods, callback);
+    else
+      callback([])
   },
   reporter: function(reporter, cite, env, callback) {
     get_from_courtlistener_search(cite, env, callback);
@@ -248,12 +259,18 @@ function get_from_courtlistener_search(cite, env, callback) {
 }
 
 // us_bill citator stub and extending linkers for citations to bills
+var us_bill_type_display = { hr: "H.R.", s: "S.", hres: "H.Res.", sres: "S.Res.", hjres: "H.J.Res.", sjres: "S.J.Res.", hconres: "H.Con.Res.", sconres: "S.Con.Res." };
+function ordinal(number) {
+  const suffixes = ["th", "st", "nd", "rd"];
+  const v = Math.abs(number) % 100;
+  return number + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
+}
 Citation.types.us_bill = {
   id: function(cite) {
     return "us_bill/" + cite.congress + "/" + cite.bill_type + "/" + cite.number;
   },
   canonical: function(cite) {
-    return cite.bill_type.toUpperCase() + " " + cite.number + " (" + cite.congress + ")";
+    return (us_bill_type_display[cite.bill_type] || cite.bill_type.toUpperCase()) + " " + cite.number + " (" + ordinal(cite.congress) + " Congress)";
   }
 };
 Citation.links.gpo.citations.us_bill = function(cite) {
